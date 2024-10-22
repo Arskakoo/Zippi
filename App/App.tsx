8fd9e58451f733a74b453c120dbb3638a2b213a0
Arska <aaron.kairavuo@gmail.com>
2024-10-22 11:32:50 +0300

    Zippi top text

--- App/App.tsx
+++ App/App.tsx
@@ -1,10 +1,11 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import { Colors } from "react-native/Libraries/NewAppScreen";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text style={styles.top}>Zippi</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -12,9 +13,14 @@ export default function App() {
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#ffff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#ffff",
+  },
+  top: {
+    color: "#000000",
+    textAlign: "center",
+    marginTop: 20,
+    fontWeight: "bold",
+    fontSize: 20,
+    padding: 6,
   },
 });
