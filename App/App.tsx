c34a544f6dcf21683121d08330182894459e37f9
Arska <aaron.kairavuo@gmail.com>
2024-10-22 11:59:28 +0300

    Committi

--- App/App.tsx
+++ App/App.tsx
@@ -1,11 +1,15 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
-import { Colors } from "react-native/Libraries/NewAppScreen";
+import { StyleSheet, Text, View, Image, Button, Pressable } from "react-native";
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text style={styles.top}>Zippi</Text>
+      <Image source={require("./assets/Order.png")} style={styles.image} />
+      <Text style={styles.welcome}>Welcome to Zippi</Text>
+      <Pressable style={styles.start}>
+        <Text style={styles.startText}>Lets Ride</Text>
+      </Pressable>
       <StatusBar style="auto" />
     </View>
   );
@@ -22,5 +26,33 @@ const styles = StyleSheet.create({
     fontWeight: "bold",
     fontSize: 20,
     padding: 6,
+  },
+  welcome: {
+    textAlign: "center",
+    fontSize: 30,
+    marginTop: 20,
+    fontWeight: "bold",
+  },
+  image: {
+    justifyContent: "center",
+    textAlign: "center",
+    margin: "auto",
+    width: 400,
+    height: 400,
+    padding: 20,
+    marginVertical: 50,
+  },
+  start: {
+    backgroundColor: "#2eb000",
+    alignSelf: "center",
+    paddingVertical: 10,
+    paddingHorizontal: 70,
+    borderRadius: 12,
+    marginTop: 20,
+  },
+  startText: {
+    fontSize: 25,
+    color: "#ffff",
+    fontWeight: "bold",
   },
 });
