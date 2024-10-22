ab87a22e73b2936119be6eec0682869f45b398a3
Samuel Nummela <Samuelnummela06@gmail.com>
2024-10-22 12:55:59 +0300

    kamaa

--- App/App.tsx
+++ App/App.tsx
@@ -4,14 +4,18 @@ import { createDrawerNavigator } from "@react-navigation/drawer";
 import HomeScreen from "./screens/HomeScreen";
 import NotificationsScreen from "./screens/NotificationsScreen";
 import ProfileScreen from "./screens/ProfileScreen";
+import CustomDrawerContent from "./CustomDrawerContent"; // Import the custom drawer content
 
 const Drawer = createDrawerNavigator();
 
 export default function App() {
   return (
     <NavigationContainer>
-      <Drawer.Navigator initialRouteName="Home">
-        <Drawer.Screen name="Zippi" component={HomeScreen} />
+      <Drawer.Navigator 
+        initialRouteName="Home" 
+        drawerContent={(props) => <CustomDrawerContent {...props} />} // Use custom drawer content
+      >
+        <Drawer.Screen name="Home" component={HomeScreen} />
         <Drawer.Screen name="Notifications" component={NotificationsScreen} />
         <Drawer.Screen name="Profile" component={ProfileScreen} />
       </Drawer.Navigator>
